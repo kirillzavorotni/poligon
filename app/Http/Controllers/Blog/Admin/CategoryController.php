6163fca33b5823585a8378c0d44346a993eba905
kirillzavorotni <kirillzavorotni@gmail.com>
2020-12-13 18:10:35 +0300

    add admin category paginator

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 
 class CategoryController extends BaseController
@@ -13,7 +14,8 @@ class CategoryController extends BaseController
      */
     public function index()
     {
-        dd(__METHOD__);
+        $paginator = BlogCategory::paginate(15);
+        return view('blog.admin.category.index', compact('paginator'));
     }
 
     /**
